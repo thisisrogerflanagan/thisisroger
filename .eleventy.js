0194fed6173acf6c2923fa0795ebad32dc408b81
Roger Flanagan <rogerflanagan@fb.com>
2025-10-17 14:50:35 -0400

    [Added] Website

---
+++ .eleventy.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/styles.css");
+  eleventyConfig.addPassthroughCopy("src/fonts");
+  eleventyConfig.addPassthroughCopy("src/assets"); // for favicon, og images, etc.
+
+  eleventyConfig.addShortcode("svg", function (filename) {
+    const filePath = `./src/assets/img/${filename}.svg`; // Adjust path as needed
+    if (fs.existsSync(filePath)) {
+      return fs.readFileSync(filePath, "utf8");
+    }
+    return ""; // Return empty string if file not found
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+      includes: "_includes",
+    },
+    templateFormats: ["njk", "md", "html"],
+    htmlTemplateEngine: "njk",
+  };
+};
---
+++ README.md
@@ -0,0 +1,13 @@
+# Meltdown Workshop
+
+Supply &amp; design since 2023. Meltdown is a small, but mighty studio, sign foundry, and goods shop created by accidental-entrepreneur and Sasquatch believer, Roger Flanagan. We make original signs and prints for people like you. Handmade in Northern Kentucky.
+
+## Development
+
+### Sass Configuration
+
+sass --watch src/scss/style.scss src/css/style.css
+
+### 11ty Configuration
+
+npm start => eleventy --serve
